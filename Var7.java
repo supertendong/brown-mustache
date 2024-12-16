06b977c942fdbf941f8c83c2fba8ed81a9468f9f
supertendong <ckdwls0519@gmail.com>
2024-12-16 09:53:07 +0900

    Add files via upload

---
+++ Var7.java
@@ -0,0 +1,24 @@
+package variable;
+
+public class Var7 {
+    public static void main(String[] args) {
+        int a = 100; //정수 리터럴
+        double b = 10.5; // 실수 리터럴
+        boolean c = true; //불리언(boolean) 리터럴 ture, false 입력 가능
+        char d = 'A'; //문자 하나 리터럴
+        String e = "Hello Java"; //문자열 리터럴, 문자열을 다루기 위한 특별한 타입
+// 리터럴 이라는 단어의 어원이 문자 또는 글자를 의미한다.
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+        /*리터럴이란
+        코드에서 개발자가 직접 적은 100, 10.5, true, 'A', "Hello Java"와
+        같은 고정된 값을 프로그래밍 용어로 리터럴이라고 한다.
+
+        */
+    }
+}
---
+++ VarEx3.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long l = 1000000000L;
+        boolean b = true;
+
+        System.out.println(l);
+        System.out.println(b);
+    }
+}
